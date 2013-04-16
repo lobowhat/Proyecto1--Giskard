217463ff509c1fb116852e4ef42c315fa1d83601
daniel7 <darayavilla@hotmail.com>
2013-04-15 23:33:47 -0600

    documentando metodos en clase individuo

--- main.cpp
+++ main.cpp
@@ -9,12 +9,12 @@ int main()
 {
     cout << "Hello Giskard!" << endl;
 //    ReconstruccionImagenes *p = new ReconstruccionImagenes();
-//    p->recibeImagenGuardada("/home/darayavilla/Qt_projects/lobowhat-Proyecto1--Giskard/raya1.jpg");//insertar path de imagen
+//    p->recibeImagenGuardada( "/home/darayavilla/Qt_projects/lobowhat-Proyecto1--Giskard/raya1.jpg" );//insertar path de imagen
     //p->recibeImagenCapturada();//inicia la captura de video
 //    p->initReconstruccionImagen();
 //    delete p;
 
-    Poblacion *po = new Poblacion(5);
+    Poblacion *po = new Poblacion(5);//se inserta un numero de individuos
     po->crearPoblacionInicial();
     po->printPoblacion();
     delete po;
