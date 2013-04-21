011b52b37859a3c249dc4556e375645f294a6841
brallan <apcomptec@gmail.com>
2013-04-20 23:28:40 -0600

    - BaseConversion adaptado para valores grandes

--- util/baseconversion.cpp
+++ util/baseconversion.cpp
@@ -1,29 +1,57 @@
 #include <cmath>
+#include <string>
+#include <iostream>
+
 #include "baseconversion.h"
 
 BaseConversion::BaseConversion()
-    : p(0), result(0)
 {
     // empty body
 }
 
-int BaseConversion::binaryToDecimal(int pNumber)
+unsigned long BaseConversion::binaryToDecimal(unsigned long pNumber)
 {
+    return conversion(pNumber, 2, 10);
+}
+
+unsigned long BaseConversion::decimalToBinary(unsigned long pNumber)
+{
+    return conversion(pNumber, 10, 2);
+}
+
+unsigned short *BaseConversion::convertToArray()
+{
+//    char* num = result + '';
+    return arrayPtr;
+}
+
+void BaseConversion::printArray() const
+{
+    std::cout << "ppp: " << p << std::endl;
+    for (int var = p - 1; var > 0; var--) {
+        std::cout << arrayPtr[var] << " ";
+    }
+
+}
+
+unsigned long BaseConversion::conversion(unsigned long pNumber, const char pFromBase, const char pToBase)
+{
+    p = 0;
+    result = 0;
+
     while (pNumber > 0) {
-        result += ((pNumber % 10) * pow(2, p++));
-        pNumber /= 10;
+        result += ((pNumber % pToBase) * pow(pFromBase, p++));
+        pNumber /= pToBase;
     }
 
     return result;
 }
 
-
-int BaseConversion::decimalToBinary(int pNumber)
-{
-    while (pNumber > 0) {
-        result += ((pNumber % 2) * pow10(p++));
-        pNumber /= 2;
-    }
-
-    return result;
-}
+//int main()
+//{
+//    BaseConversion p;
+//    std::cout << (p.decimalToBinary(1000)) << std::endl;
+//    std::cout << (p.binaryToDecimal(10)) << std::endl;
+//    p.convertToArray();
+//    p.printArray();
+//}
--- util/baseconversion.h
+++ util/baseconversion.h
@@ -5,12 +5,25 @@ class BaseConversion
 {
 public:
     BaseConversion(); // constructor
-    int binaryToDecimal(int pNumber); // convierte de binario a decimal
-    int decimalToBinary(int pNumber); // convierte de decimal a binario
+
+    // convierte de binario a decimal
+    unsigned long binaryToDecimal(unsigned long pNumber);
+
+    // convierte de decimal a binario
+    unsigned long decimalToBinary(unsigned long pNumber);
+
+    unsigned short *convertToArray();
+
+    void printArray() const;
 
 private:
-    int p; // valores de potencia
-    int result; // resultado
+    unsigned short p; // valores de potencia
+    unsigned long result; // resultado
+    unsigned short *arrayPtr; // puntero al arreglo equivalente del número
+
+    // función de conversion
+    unsigned long conversion(unsigned long pNumber, const char pFromBase,
+                             const char pToBase);
 };
 
 #endif // BASECONVERSION_H
