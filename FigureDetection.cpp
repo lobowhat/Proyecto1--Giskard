2b5c2d1e40efca68ecbc04316f78b102798d322b
Jose Daniel <josebaduma@gmail.com>
2013-04-25 09:09:08 -0600

    Deteccion figuras

--- FigureDetection.cpp
+++ FigureDetection.cpp
@@ -1,34 +1,32 @@
 #include "FigureDetection.h"
 
-IplImage* FigureDetection::get(IplImage* img){
-    CvMemStorage* storage = cvCreateMemStorage(0);
-    CvSeq* contour;
-    CvSeq* sec;
+FigureDetection::FigureDetection(){
+     this->storage = cvCreateMemStorage(0);
+}
+FigureDetection::~FigureDetection(){
+    cvReleaseImage( &this->grayImage );//Libera la memoria de la imagen
+    cvReleaseImage( &this->copyImage );//Libera la memoria de la imagen
+}
 
-    IplImage *copyImage, *grayImage;//Imagenes
-    IplImage* ret = cvCloneImage(img);
+IplImage* FigureDetection::get(IplImage* pImg){
 
-    grayImage = cvCreateImage( cvGetSize(img), IPL_DEPTH_8U, 1 );//Creacion de una imagen
-    cvCvtColor( img, grayImage, CV_BGR2GRAY);//Convertir imagen original a escala de grises
+    IplImage* ret = cvCloneImage(pImg);
 
-    copyImage = cvCreateImage(cvGetSize(img), IPL_DEPTH_8U, 1);//Creacion de una imagen
-    cvCanny(grayImage,copyImage,50,150);//Conversion de la imagen en contornos
+    this->getImageContour(pImg);
 
-    //Encuentra los contornos
-    cvFindContours(copyImage, storage, &contour, sizeof(CvContour), CV_RETR_LIST, CV_CHAIN_APPROX_SIMPLE, cvPoint(0,0));
-    cout << "Se encontro contornos"<<endl;
-
-    while(contour){//Ciclo de busquedas de contornos
+    while(this->contour){//Ciclo de busquedas de contornos
 
         //Se obtienen las secuencias
-        sec = cvApproxPoly(contour, sizeof(CvContour), storage, CV_POLY_APPROX_DP, cvContourPerimeter(contour)*0.02,0);
+        this->sec = cvApproxPoly(this->contour, sizeof(CvContour), storage, CV_POLY_APPROX_DP, cvContourPerimeter(this->contour)*0.02,0);
 
         //Verificacion en caso de ser cuadrilateros
-        if(sec->total == 4 && fabs(cvContourArea(sec, CV_WHOLE_SEQ)) > 30){
+        if(this->sec->total == 4 && fabs(cvContourArea(this->sec, CV_WHOLE_SEQ)) > 30){
             CvPoint* ptr[4];
             for(int i = 0; i<4; i++){
-                ptr[i] = (CvPoint*)cvGetSeqElem(sec,i);
+                ptr[i] = (CvPoint*)cvGetSeqElem(this->sec,i);
             }
+
+            this->printCharateristic(ptr, 4);
 
             cvLine(ret, *ptr[0], *ptr[1], CV_RGB(255,0,255),3);
             cvLine(ret, *ptr[1], *ptr[2], CV_RGB(255,0,255),3);
@@ -37,37 +35,62 @@ IplImage* FigureDetection::get(IplImage* img){
         }
 
         //Verificacion en caso de ser triangulo
-        if(sec->total == 3 && fabs(cvContourArea(sec, CV_WHOLE_SEQ)) > 30){
+        //
+        if(this->sec->total == 3 && fabs(cvContourArea(this->sec, CV_WHOLE_SEQ)) > 30){
             CvPoint* ptr[3];
             for (int i = 0; i < 3; ++i) {
-                ptr[i] = (CvPoint*)cvGetSeqElem(sec,i);
+                ptr[i] = (CvPoint*)cvGetSeqElem(this->sec,i);
             }
+            cout << endl;
+            this->printCharateristic(ptr, 3);
             cvLine(ret, *ptr[0], *ptr[1], CV_RGB(255,255,0),3);
             cvLine(ret, *ptr[1], *ptr[2], CV_RGB(255,255,0),3);
             cvLine(ret, *ptr[2], *ptr[0], CV_RGB(255,255,0),3);
         }
-        contour = contour->h_next;
+        this->contour = this->contour->h_next;
     }
-    cvReleaseImage( &grayImage );//Libera la memoria de la imagen
-    cvReleaseImage( &copyImage );//Libera la memoria de la imagen
 
     return ret;
 }
 
 void FigureDetection::webCam(int pCamara){
     IplImage* frame; //Se define la imagen
-        cvNamedWindow("Color detection", CV_WINDOW_AUTOSIZE); //Se crea la ventana
-        CvCapture* capture = cvCaptureFromCAM(pCamara); //Se inicializa la captura de imagenes en la camara.
-        frame = cvQueryFrame(capture); //Se hace una captura inicial
+    cvNamedWindow("Color detection", CV_WINDOW_AUTOSIZE); //Se crea la ventana
+    CvCapture* capture = cvCaptureFromCAM(pCamara); //Se inicializa la captura de imagenes en la camara.
+    frame = cvQueryFrame(capture); //Se hace una captura inicial
 
-        int input;
-        while ((input = cvWaitKey(50)) != 27)
-        { //Ciclo de capturas de la camara
-            frame = cvQueryFrame(capture); //Se obtiene la imagen a partir de la camara
-            frame = get(frame); //Se llama a la funcion para que defina los contornos e identifique las figuras
-            cvShowImage("Color detection", frame); //Se muestra en pantalla la captura
-        }
-        cvDestroyAllWindows(); // Destruye toda ventana
-        cvReleaseCapture(&capture);
-        cvReleaseImage(&frame);
+    int input;
+    while ((input = cvWaitKey(50)) != 27)
+    { //Ciclo de capturas de la camara
+        frame = cvQueryFrame(capture); //Se obtiene la imagen a partir de la camara
+        frame = get(frame); //Se llama a la funcion para que defina los contornos e identifique las figuras
+        cvShowImage("Color detection", frame); //Se muestra en pantalla la captura
+    }
+    cvDestroyAllWindows(); // Destruye toda ventana
+    cvReleaseCapture(&capture);
+    cvReleaseImage(&frame);
+}
+
+void FigureDetection::getImageContour(IplImage* pImg){
+
+    this->grayImage = cvCreateImage( cvGetSize(pImg), IPL_DEPTH_8U, 1 );//Creacion de una imagen
+    cvCvtColor( pImg, this->grayImage, CV_BGR2GRAY);//Convertir imagen original a escala de grises
+
+    this->copyImage = cvCreateImage(cvGetSize(pImg), IPL_DEPTH_8U, 1);//Creacion de una imagen
+    cvCanny(this->grayImage,this->copyImage,50,150);//Conversion de la imagen en contornos
+
+    //Encuentra los contornos
+    cvFindContours(this->copyImage, storage, &this->contour, sizeof(CvContour), CV_RETR_LIST, CV_CHAIN_APPROX_SIMPLE, cvPoint(0,0));
+}
+
+void FigureDetection::printCharateristic(CvPoint ** pPoints, int pSides){
+    if(pSides == 4){
+        cout << "Ancho: "<<(pPoints[1]->x - pPoints[0]->x)<<" Largo: "<<(pPoints[2]->y-pPoints[1]->y) << endl;
+    }else if(pSides == 3){
+        int base, altura;
+        base = sqrt(pow((pPoints[2]->x - pPoints[0]->x),2) + pow((pPoints[2]->y - pPoints[0]->y),2));
+        altura = sqrt(pow((1),2) + pow((1),2));
+        cout << "Base: "<<base<<" Altura: "<<altura << endl;
+    }
+
 }
--- FigureDetection.h
+++ FigureDetection.h
@@ -4,6 +4,7 @@
 #include <highgui.h>
 #include <cv.h>
 #include <iostream>
+#include <cmath>
 #include <stdio.h>
 
 using namespace std;
@@ -11,10 +12,18 @@ using namespace cv;
 
 class FigureDetection{
 private:
+    CvMemStorage* storage;
+    CvSeq* contour;
+    CvSeq* sec;
+    IplImage *copyImage, *grayImage;//Imagenes
 
 public:
+    FigureDetection();
+    ~FigureDetection();
     IplImage* get(IplImage*);
+    void getImageContour(IplImage*);
     void webCam(int);
+    void printCharateristic(CvPoint**, int);
 };
 
 
